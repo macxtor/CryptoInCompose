e2b650c59975f2a146b66aab06909f7d22b85016
Aamir Choksi <aamirchoksi@gmail.com>
2023-12-10 22:54:07 +0000

    initate dagger and navigation

---
+++ app/src/main/java/com/example/cryptoincompose/CryptoComposeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.cryptoincompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoComposeApplication : Application()
--- app/src/main/java/com/example/cryptoincompose/MainActivity.kt
+++ app/src/main/java/com/example/cryptoincompose/MainActivity.kt
@@ -3,44 +3,29 @@ package com.example.cryptoincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cryptoincompose.ui.theme.CryptoInComposeTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptoincompose.presentation.screens.home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CryptoInComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoInComposeTheme {
-        Greeting("Android")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen()
+        }
     }
 }
